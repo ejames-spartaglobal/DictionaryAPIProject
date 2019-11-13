cd40d87f55ccd578ed91686c526cef77006a03fc
ejames-spartaglobal <55740324+ejames-spartaglobal@users.noreply.github.com>
2019-11-13 09:57:17 +0000

    Delete APIKeyAndID.java

--- src/test/java/com/spartaglobal/oxford_dictionary_api_project/config/APIKeyAndID.java
+++
@@ -1,6 +0,0 @@
-package com.spartaglobal.oxford_dictionary_api_project.config;
-
-public class APIKeyAndID {
-    public final static String appKey= "e2e22af3f725aafe64eca3440d085036";
-    public final static String appID= "323147e8";
-}
